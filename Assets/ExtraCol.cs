ac008701072ce7c543b83b993e4fb8fff8869789
Alexander Uddfeldt <alexander.uddfeldt@gmail.com>
2014-09-28 14:55:12 +0200

    hela projectet

---
+++ Assets/ExtraCol.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExtraCol : MonoBehaviour {
+
+	public float moveSpeed;
+	public GameObject[] reds = GameObject.FindGameObjectsWithTag("Red");
+	// Use this for initialization
+	void Start () {
+		transform.position = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float h = Input.GetAxis ("horizontal") * moveSpeed;
+		transform.Translate (Vector2.right * h * Time.deltaTime);
+	}
+	void OnCollisionEnter2D(Collision2D col){
+		if (col.gameObject.tag == "Red") {
+			transform.position = new Vector3(20,1,0);
+		}
+	}
+}
